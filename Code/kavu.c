bf9bef23f82da23f104666981deb019d0758ec91
pavo9001 <p.cortes9000@gmail.com>
2026-03-03 17:51:00 -0600

    enemies now one-hit the player in gambit perfect chrome finish, bump map version number

--- Code/kavu.c
+++ Code/kavu.c
@@ -1,5 +1,46 @@
 #include <libdl/moby.h>
+#include <libdl/game.h>
+#include <libdl/stdlib.h>
 #include "game.h"
+#include "interop.h"
+#include "shared.h"
+
+float mapGetCurrentDifficulty(void);
+
+//--------------------------------------------------------------------------
+void populateSpawnArgsFromConfigCustom(struct MobSpawnEventArgs* output, struct MobConfig* config, int spawnParamsIdx, int isBaseConfig, int spawnFlags) {
+  GameSettings* gs = gameGetSettings();
+  if (!gs) return;
+
+  float speed = config->Speed;
+  float health = config->Health;
+  float difficulty = mapGetCurrentDifficulty();
+
+  if (isBaseConfig) {    
+    speed = speed * (1 + (MOB_BASE_SPEED_SCALE * config->SpeedScale * difficulty));
+    health = health * powf(1 + (MOB_BASE_HEALTH_SCALE * config->HealthScale * difficulty), 2);
+  }
+
+  // enforce max values
+  if (config->MaxSpeed > 0 && speed > config->MaxSpeed) speed = config->MaxSpeed;
+  if (config->MaxHealth > 0 && health > config->MaxHealth) health = config->MaxHealth;
+  
+  output->SpawnParamsIdx = spawnParamsIdx;
+  output->Bolts = config->Bolts;
+  output->Xp = config->Xp;
+  output->StartHealth = health;
+  output->Bangles = (u16)config->Bangles;
+  output->Damage = 10000;
+  output->AttackRadiusEighths = (u8)(config->AttackRadius * 8);
+  output->HitRadiusEighths = (u8)(config->HitRadius * 8);
+  output->CollRadiusEighths = (u8)(config->CollRadius * 8);
+  output->SpeedEighths = (u16)(speed * 8);
+  output->ReactionTickCount = (u8)config->ReactionTickCount;
+  output->AttackCooldownTickCount = (u8)config->AttackCooldownTickCount;
+  output->DamageCooldownTickCount = (u16)config->DamageCooldownTickCount;
+  output->MobAttribute = config->MobAttribute;
+  output->Behavior = config->Behavior;
+}
 
 //--------------------------------------------------------------------------
 void forceOnAllDemonBells(void) {
@@ -22,4 +63,12 @@ void forceOnAllDemonBells(void) {
 //--------------------------------------------------------------------------
 void gambitsPerfectChromeInit(void) {
   forceOnAllDemonBells();
+}
+
+void gambitsPerfectChromeTick(void) {
+  static int init = 0;
+	if (!init && MapConfig.ClientsReady) {
+		init = 1;
+		MapConfig.Functions.ModePopulateSpawnArgsFunc = &populateSpawnArgsFromConfigCustom;
+	}
 }
